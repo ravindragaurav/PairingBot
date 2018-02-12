b56cbbe12e7d17eb22a4e186fb82cec46c4a2772
Gaurav Ravindra <ravindra_gaurav@yahoo.com>
2018-02-12 21:19:55 +0000

    initial idea for a pairing bot

---
+++ src/PairingBot.Client/Pairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PairingBot.Client
+{
+    public class Pairs
+    {
+        readonly List<string> _pairs;
+
+        public Pairs(List<string> pairs)
+        {
+            _pairs = pairs;
+        }
+
+        public string GetPairs()
+        {
+            var random = _pairs.OrderBy(arg => Guid.NewGuid()).Take(2).ToList(); //you can randomise lists by using Guids!!
+            
+            return $"Today's pairs are : {random[0]} & {random[1]}";
+        }
+    }
+}
---
+++ src/PairingBot.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PairingBot.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlWithAccessToken = "";
+
+            SlackClient client = new SlackClient(urlWithAccessToken);
+
+            List<string> teamMembers = new List<string>();
+
+            teamMembers.Add("Gaurav");
+            teamMembers.Add("Aash");
+            teamMembers.Add("Doug");
+            teamMembers.Add("John");
+
+            var pairs = new Pairs(teamMembers);
+            var result = pairs.GetPairs();
+            client.PostMessage(username: "Gaurav",
+                       text: result,
+        channel: "#cobrapairs");
+        }
+    }
+}
